5279347ee971a9d34bda949f7825ccfb8c44fcfd
Дарина Ватутіна <darinavatutina527@gmail.com>
2026-04-09 04:03:32 +0300

    Refactor Observer interface and Observable class

--- Pack7/Observer.java
+++ Pack7/Observer.java
@@ -3,32 +3,6 @@ package Pack7;
 import java.lang.annotation.*;
 import java.util.*;
 
-/** Анотація для спостерігачів (RUNTIME retention) */
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-@Documented
-@interface ObserverInfo {
-    String name() default "Unknown";
-    String description() default "Observer";
-}
-
-/** Анотація для методів, що реагують на зміни (CLASS retention) */
-@Retention(RetentionPolicy.CLASS)
-@Target(ElementType.METHOD)
-@Documented
-@interface Watcher {
-    String name() default "DefaultWatcher";
-    String action() default "update";
-}
-
-/** Анотація для критичних методів (SOURCE retention - видалюється компілятором) */
-@Retention(RetentionPolicy.SOURCE)
-@Target(ElementType.METHOD)
-@Documented
-@interface CriticalMethod {
-    String note() default "";
-}
-
 /** Інтерфейс спостерігача */
 public interface Observer {
     void update(List<Item2d> items);
